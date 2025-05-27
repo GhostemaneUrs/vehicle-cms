3694e6c5c017feb5342c4aba72a74d246b25ed1a
Andres Jaramillo <JaramilloG1999@outlook.com>
2025-05-27 08:46:53 -0500

    feat: Add organizational module, including controllers, services, entities and DTOs. Implement organizational management logic and relationships in the database.

---
+++ src/organizational/entities/organizational.entity.ts
@@ -0,0 +1,38 @@
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+  CreateDateColumn,
+  ManyToOne,
+  ManyToMany,
+  JoinTable,
+  OneToMany,
+  Index,
+} from 'typeorm';
+import { Project } from '../../projects/entities/project.entity';
+import { User } from '../../auth/entities/user.entity';
+import { Transfer } from '../../transfers/entities/transfer.entity';
+
+@Entity('organizational')
+@Index('idx_organizational_name', ['name'])
+export class Organizational {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column()
+  name: string;
+
+  @Column({ type: 'timestamp', default: () => 'now()', name: 'created_at' })
+  createdAt: Date;
+
+  @ManyToOne(() => Project, (project) => project.organizational, {
+    onDelete: 'CASCADE',
+  })
+  project: Project;
+
+  @ManyToMany(() => User, (user) => user.organizational)
+  users: User[];
+
+  @OneToMany(() => Transfer, (t) => t.organizational)
+  transfers: Transfer[];
+}
