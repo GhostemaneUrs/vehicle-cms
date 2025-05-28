2cd5398fd237fe532522fc95104207f3b708ef4b
Andres Jaramillo <JaramilloG1999@outlook.com>
2025-05-28 11:14:33 -0500

    feat: update docker

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,11 +1,9 @@
 #!/bin/sh
 set -e
 
-# si no viene POSTGRES_PORT, por defecto 5432
 : "${POSTGRES_PORT:=5432}"
 
 echo "⏳ Waiting for database at $POSTGRES_HOST:$POSTGRES_PORT…"
-# hasta que abra el puerto, duerme 1s
 while ! nc -z "$POSTGRES_HOST" "$POSTGRES_PORT"; do
   sleep 1
 done
@@ -14,7 +12,7 @@ echo "🚜 Running database migrations…"
 npm run db:migrate:prod
 
 echo "🌱 Seeding initial data…"
-npm run db:seed:prod
+npm run db:seed
 
 echo "🚀 Starting application…"
 exec npm run start:prod
